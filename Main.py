f8147513ccf43509a425be294b4fa6eb456e8bc5
Morse <enmorse48@gmail.com>
2021-09-19 21:41:48 -0700

    Initial commit

---
+++ Main.py
@@ -0,0 +1,10 @@
+# Assign a message to a variable and print that message.
+
+# Then change the value of that variable to a new message
+# and print that message.
+
+message = "Hello, World!"
+print(message)
+
+message = "Hello, Python World!"
+print(message)
